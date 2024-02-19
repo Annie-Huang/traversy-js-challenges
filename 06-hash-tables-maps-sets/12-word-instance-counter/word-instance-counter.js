d92deaca778bff2c93ae33f6bfbd05c3bbce6a89
Annie Huang <annie.hn@gmail.com>
2024-02-19 12:09:34 +1100

    Finished '06-hash-tables-maps-sets\12-word-instance-counter'

---
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -0,0 +1,22 @@
+const HashTable = require('./HashTable');
+
+function wordInstanceCounter(str, word) {
+  const arr = str.toLowerCase().split(/\W+/);
+
+  const hashTable = new HashTable();
+  arr.map((item) => {
+    if (item === '') return;
+
+    if (!hashTable.get(item)) {
+      hashTable.set(item, 1);
+    } else {
+      hashTable.set(item, hashTable.get(item) + 1);
+    }
+  });
+
+  return hashTable.get(word.toLocaleString());
+}
+
+module.exports = wordInstanceCounter;
+
+split(/\W+/);
