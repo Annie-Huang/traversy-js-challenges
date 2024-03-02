a18eed9a2fb2db5e73c1112c6613118788ea85e0
Annie Huang <annie.hn@gmail.com>
2024-03-02 15:33:46 +1100

    Read '08-binary-trees-graphs\06-maximum-depth'

---
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+}
+
+module.exports = {
+  maxDepth,
+  Node,
+};
