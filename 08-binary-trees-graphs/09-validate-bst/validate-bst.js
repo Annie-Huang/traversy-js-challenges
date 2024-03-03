6b6a1b864876909c033e0a46314a69cf24038f51
Annie Huang <annie.hn@gmail.com>
2024-03-03 16:09:38 +1100

    Read '08-binary-trees-graphs\09-validate-bst'

---
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function isValidBST(root) {
+  const isValidBST = (root, min = null, max = null) => {
+    if (root === null) {
+      return true;
+    }
+
+    if (
+      (min !== null && root.value <= min) ||
+      (max !== null && root.value >= max)
+    ) {
+      return false;
+    }
+
+    return (
+      isValidBST(root.left, min, root.value) &&
+      isValidBST(root.right, root.value, max)
+    );
+  };
+
+  return isValidBST(root);
+}
+
+module.exports = { Node, isValidBST };
